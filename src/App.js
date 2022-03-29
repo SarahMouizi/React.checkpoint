4620539402577121cc087ae9038a8a8abf03cb64
SarahMouizi <sarah.mouizi@gmail.com>
2022-03-29 15:14:51 +0100

    my first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import LoginPage from './Pages/loginPage'
+
+function App() {
+  return (
+    <div className="App">
+
+      <LoginPage/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/loginPage.js
@@ -0,0 +1,14 @@
+import LoginForm from '../components/loginForm'
+import Footer from '../components/footer'
+import React from 'react'
+
+const loginPage = () => {
+  return (
+      <>
+        <LoginForm/>
+        <Footer/>
+      </>
+  )
+}
+
+export default loginPage
